0dd74f384eebfd7aadb4b13c78cadccdb97e4f17
MichaelSwiatkiwsky <michael@swiatkiwsky.com>
2018-12-01 21:33:35 +1100

    Solved challenge 1A

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+
+        List<String> lines = Files.readAllLines(Paths.get("input/input1A.txt"));
+
+        int sum = 0;
+
+        for (String line : lines) {
+            sum += Integer.parseInt(line);
+        }
+
+        System.out.println(sum);
     }
 }
