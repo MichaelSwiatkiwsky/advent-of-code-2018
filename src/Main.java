4a9cbfe6ce102902a2ce70a7fdcc2f9ee9658318
MichaelSwiatkiwsky <michael@swiatkiwsky.com>
2018-12-02 16:46:42 +1100

    Solved Day 2 puzzles

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Challenge 1A solution: " + solveChallenge1A("input/input1A.txt"));
         System.out.println("Challenge 1B solution: " + solveChallenge1B("input/input1A.txt"));
+        System.out.println("Challenge 2A solution: " + solveChallenge2A("input/input2A.txt"));
+        System.out.println("Challenge 2B solution: " + solveChallenge2B("input/input2A.txt"));
     }
 
     public static int solveChallenge1A(String inputFileName) throws IOException {
@@ -41,6 +43,63 @@ public class Main {
                 seen.put(sum, seen.getOrDefault(sum, 0) + 1);
             }
         }
+    }
+
+    public static int solveChallenge2A(String inputFileName) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(inputFileName));
+        int doubleCount = 0;
+        int tripleCount = 0;
+
+        for (String line : lines) {
+            Map<Character,Integer> seen = new HashMap<Character, Integer>();
+            for (int i = 0; i < line.length(); i++) {
+                Character c = line.charAt(i);
+                seen.put(c, seen.getOrDefault(c, 0) + 1);
+            }
+            boolean foundDouble = false;
+            boolean foundTriple = false;
+
+            for (Map.Entry<Character, Integer> entry : seen.entrySet()) {
+                if (entry.getValue() == 2 && !foundDouble) {
+                    foundDouble = true;
+                    doubleCount++;
+                }
+                if (entry.getValue() == 3 && !foundTriple) {
+                    foundTriple = true;
+                    tripleCount++;
+                }
+            }
+        }
+
+        return doubleCount * tripleCount;
+    }
+
+    public static String solveChallenge2B(String inputFileName) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(inputFileName));
+
+        int ID_SIZE = lines.get(0).length();
+
+        // Is this N^2 or NlogN complexity? Can this be made more efficient?
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = i + 1; j < lines.size(); j++) {
+                String strA = lines.get(i);
+                String strB = lines.get(j);
+
+                String similarChars = "";
+
+                for (int a = 0; a < strA.length(); a++) {
+                    if (strA.charAt(a) == strB.charAt(a)) {
+                        similarChars += strA.charAt(a);
+                    }
+                }
+                if (similarChars.length() == ID_SIZE - 1) {
+                    return similarChars;
+                }
+            }
+        }
+
+        // Should never be reached if input was valid
+        return "invalid";
     }
 }
 
