c3af9e3f7d7934bf1280e6dcbd7e961385a5523e
MichaelSwiatkiwsky <michael@swiatkiwsky.com>
2018-12-04 13:59:33 +1100

    Solved day 3

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ public class Main {
         System.out.println("Challenge 1B solution: " + solveChallenge1B("input/input1A.txt"));
         System.out.println("Challenge 2A solution: " + solveChallenge2A("input/input2A.txt"));
         System.out.println("Challenge 2B solution: " + solveChallenge2B("input/input2A.txt"));
+        System.out.println("Challenge 3A solution: " + solveChallenge3A("input/input3A.txt"));
+        System.out.println("Challenge 3B solution: " + solveChallenge3B("input/input3A.txt"));
     }
 
     public static int solveChallenge1A(String inputFileName) throws IOException {
@@ -107,6 +109,115 @@ public class Main {
 
         // Should never be reached if input was valid
         return "invalid";
+    }
+
+    public static int solveChallenge3A(String inputFileName) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(inputFileName));
+
+        Map<Integer, Map<Integer, Integer>> claimed = new HashMap<>();
+
+        for (String line : lines) {
+            String[] parts = line.split(" ");
+
+            String[] coords = parts[2].split(",");
+            int xPos = Integer.parseInt(coords[0]);
+            int yPos = Integer.parseInt(coords[1].substring(0, coords[1].length() - 1));
+
+            String[] dimensions = parts[3].split("x");
+            int width = Integer.parseInt(dimensions[0]);
+            int height = Integer.parseInt(dimensions[1]);
+
+            for (int j = 0; j < height; j++) {
+                for (int i = 0; i < width; i++) {
+                    int x = xPos + i;
+                    int y = yPos + j;
+                    if (claimed.get(x) == null) {
+                        claimed.put(x, new HashMap<>());
+                    }
+
+                    claimed.get(x).put(y, claimed.get(x).getOrDefault(y, 0) + 1);
+                }
+            }
+        }
+
+        int numOverlappingSquares = 0;
+
+        for (Map<Integer, Integer> innerMap : claimed.values()) {
+            for (int coordCount : innerMap.values()) {
+                if (coordCount > 1) {
+                    numOverlappingSquares++;
+                }
+            }
+        }
+
+        return numOverlappingSquares;
+    }
+
+    /** This works, but it is a "dumb" solution - essentially just brute force. There is surely a smarter way
+     * to calculate overlaps mathematically without relying on a hashmap of "seen" coordinates.
+     *
+     * An alternate approach could be to compare each rectangle against each other and check mathematically if they
+     * overlap (based on xPos, yPos, width and height) but that would also require multiple passes of list. I feel like
+     * there is probably a way to do it with just one pass - maybe by keeping track of perimeters.**/
+    public static int solveChallenge3B(String inputFileName) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(inputFileName));
+
+        Map<Integer, Map<Integer, Integer>> claimed = new HashMap<>();
+
+        for (String line : lines) {
+            String[] parts = line.split(" ");
+
+            String[] coords = parts[2].split(",");
+            int xPos = Integer.parseInt(coords[0]);
+            int yPos = Integer.parseInt(coords[1].substring(0, coords[1].length() - 1));
+
+            String[] dimensions = parts[3].split("x");
+            int width = Integer.parseInt(dimensions[0]);
+            int height = Integer.parseInt(dimensions[1]);
+
+            for (int j = 0; j < height; j++) {
+                for (int i = 0; i < width; i++) {
+                    int x = xPos + i;
+                    int y = yPos + j;
+                    if (claimed.get(x) == null) {
+                        claimed.put(x, new HashMap<>());
+                    }
+
+                    claimed.get(x).put(y, claimed.get(x).getOrDefault(y, 0) + 1);
+                }
+            }
+        }
+
+        for (int a = 0; a < lines.size(); a++) {
+            String[] parts = lines.get(a).split(" ");
+
+            String[] coords = parts[2].split(",");
+            int xPos = Integer.parseInt(coords[0]);
+            int yPos = Integer.parseInt(coords[1].substring(0, coords[1].length() - 1));
+
+            String[] dimensions = parts[3].split("x");
+            int width = Integer.parseInt(dimensions[0]);
+            int height = Integer.parseInt(dimensions[1]);
+
+            boolean noOverlaps = true;
+            // By checking for noOverlaps we can break out of outer loop too once overlap found
+            for (int j = 0; j < height && noOverlaps; j++) {
+                for (int i = 0; i < width; i++) {
+                    int x = xPos + i;
+                    int y = yPos + j;
+                    if (claimed.get(x).get(y) > 1) {
+                        noOverlaps = false;
+                        break; // Leave early when we know there is overlap
+                    }
+                }
+            }
+            if (noOverlaps) {
+                return a + 1; // +1 because a starts at 0 but IDs start at 1
+            }
+        }
+
+        // Should never reach here if input valid
+        return -1;
     }
 }
 
